af24f246a3edcabbdab09f0ed160edbf49f57aaa
Chase Adams <chasebuildsoftware@gmail.com>
2016-01-23 09:45:11 -0800

    Update zsh prompt to not have a slash

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -47,7 +47,7 @@ need_push () {
 }
 
 directory_name() {
-  echo "%{$fg_bold[cyan]%}%1/%\/%{$reset_color%}"
+  echo "%{$fg_bold[cyan]%}%1/%{$reset_color%}"
 }
 
 export PROMPT=$'$(directory_name) $(git_dirty)$(need_push)\n› '
