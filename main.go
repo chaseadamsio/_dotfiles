6e6b544bd37da6dd7bc6304a8bc9b1e037b18d3e
Chase Adams <realchaseadams@gmail.com>
2016-08-20 16:09:58 -0700

    updates to use go-github for github api calls

--- main.go
+++ main.go
@@ -1,234 +1,39 @@
 package main
 
 import (
-	"encoding/json"
 	"errors"
 	"flag"
 	"fmt"
 	"io/ioutil"
-	"net/http"
+	"log"
 	"os"
 	"os/exec"
-	"path/filepath"
+
+	"golang.org/x/oauth2"
+
+	"github.com/google/go-github/github"
 )
 
-type Github struct {
-	URI        string
-	APIVersion string
-	user       string
-	token      string
-	Repos      Repos
+const (
+	version = "1.0.0"
+)
+
+var client *github.Client
+
+type Fork struct {
+	Name          string
+	FullName      string
+	Owner         string
+	DefaultBranch string
+	UpstreamOwner string
 }
 
-type Repo struct {
-	ID            int    `json:"id"`
-	IsFork        bool   `json:"fork"`
-	Repo          string `json:"name"`
-	FullName      string `json:"full_name"`
-	DefaultBranch string `json:"default_branch"`
-	UpstreamName  string
-}
-
-type branch struct {
-	Commit struct {
-		SHA string `json:"sha"`
-	} `json:"commit"`
-}
-
-type Repos []Repo
-
-func (gh *Github) get(path string) ([]byte, error) {
-	requestURL := gh.URI + path
-	HTTPClient := new(http.Client)
-	*HTTPClient = *http.DefaultClient
-	req, err := http.NewRequest("GET", requestURL, nil)
-	if err != nil {
-		return nil, err
-	}
-	req.SetBasicAuth(gh.token, "")
-	resp, err := HTTPClient.Do(req)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-
-	return ioutil.ReadAll(resp.Body)
-}
-
-func (gh *Github) GetRepos() error {
-	fmt.Printf("Retrieving information about %s repos", gh.user)
-	path := filepath.Join("/users", gh.user, "repos")
-	body, err := gh.get(path)
-	if err != nil {
-		return err
-	}
-
-	json.Unmarshal(body, &gh.Repos)
-
-	for idx, repo := range gh.Repos {
-		if repo.IsFork {
-			err = gh.GetForkUpstream(&gh.Repos[idx])
-			if err != nil {
-				return err
-			}
-		}
-	}
-
-	return nil
-}
-
-func (gh *Github) GetForkUpstream(repo *Repo) error {
-	path := filepath.Join("/repos", repo.FullName)
-	body, err := gh.get(path)
-	if err != nil {
-		return err
-	}
-
-	var repoResp struct {
-		Parent struct {
-			Owner struct {
-				Login string `json:"login"`
-			} `json:"owner"`
-		} `json:"parent"`
-	}
-
-	json.Unmarshal(body, &repoResp)
-	repo.UpstreamName = repoResp.Parent.Owner.Login + "/" + repo.Repo
-	return nil
-}
-
-func (gh *Github) GetBranchLatestCommit(repoFullName string, branchName string) (*branch, error) {
-	path := filepath.Join("/repos", repoFullName, "branches", branchName)
-	body, err := gh.get(path)
-	if err != nil {
-		return nil, err
-	}
-
-	newBranch := new(branch)
-
-	json.Unmarshal(body, &newBranch)
-	return newBranch, nil
-}
-
-func (gh *Github) isCommitSynced(repo *Repo) (bool, error) {
-	fork, err := gh.GetBranchLatestCommit(repo.FullName, repo.DefaultBranch)
-	if err != nil {
-		return false, err
-	}
-
-	upstream, err := gh.GetBranchLatestCommit(repo.UpstreamName, repo.DefaultBranch)
-	if err != nil {
-		return false, err
-	}
-
-	if &fork.Commit == &upstream.Commit {
-		return true, nil
-	}
-
-	return false, nil
-}
-
-func (gh *Github) updateFork(repo *Repo) error {
-	isSynced, err := gh.isCommitSynced(repo)
-	if err != nil {
-		return err
-	}
-
-	if isSynced {
-		fmt.Printf("%s is already synced with upstream", repo.FullName)
-		return nil
-	}
-	// Create a temporary directory
-	dir, err := ioutil.TempDir("", "tmp")
-	if err != nil {
-		return err
-	}
-	defer os.Remove(dir)
-	fmt.Printf("Beginning the process of updating %s in %s", repo.FullName, dir)
-
-	// Clone the git repo into the temporary directory
-	forkURL := fmt.Sprintf("git@github.com:%s.git", repo.FullName)
-	err = gitExec([]string{"clone", "--depth", "1", forkURL, dir})
-	if err != nil {
-		return err
-	}
-	fmt.Printf("Cloned %s into %s", repo.FullName, dir)
-
-	// Change into the temporary directcory
-	err = os.Chdir(dir)
-	if err != nil {
-		return err
-	}
-
-	upstreamURL := fmt.Sprintf("git@github.com:%s.git", repo.UpstreamName)
-	err = gitExec([]string{"remote", "add", "upstream", upstreamURL})
-	if err != nil {
-		return err
-	}
-	fmt.Printf("Added upstream remote for %s", repo.UpstreamName)
-
-	err = gitExec([]string{"remote", "-v", "update", "-p"})
-	if err != nil {
-		return err
-	}
-	fmt.Printf("Updated remote for upstream %s", repo.UpstreamName)
-
-	upstream := fmt.Sprintf("upstream/%s", repo.DefaultBranch)
-	err = gitExec([]string{"rebase", upstream})
-	if err != nil {
-		return err
-	}
-	fmt.Printf("Rebased off of upstream %s", repo.UpstreamName)
-
-	err = gitExec([]string{"push", "origin", repo.DefaultBranch})
-	if err != nil {
-		return err
-	}
-	fmt.Printf("Pushed %s origin/%s", repo.FullName, repo.DefaultBranch)
-	return nil
-}
-
-func (gh *Github) updateForks() error {
-	for idx, repo := range gh.Repos {
-		if !repo.IsFork {
-			continue
-		}
-
-		err := gh.updateFork(&gh.Repos[idx])
-		if err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
-
-func gitExec(args []string) error {
-	cmd := exec.Command("git", args...)
-	output, err := cmd.CombinedOutput()
-	if err != nil {
-		return err
-	}
-	if len(output) > 0 {
-		fmt.Println(string(output))
-	}
-	return nil
-}
-
-func main() {
+func init() {
 	var (
-		uri        = flag.String("uri", "https://api.github.com", "The Github URI to update forks on")
-		apiVersion = flag.String("api-version", "v3", "The API version for the Github uri to update forks on")
-		user       = flag.String("user", "", "The GIthub user to update forks on")
-		token      = flag.String("token", "", "The github user token to update fork on")
+		token = flag.String("token", "", "The github user token to update fork on")
 	)
 
 	flag.Parse()
-
-	if *user == "" {
-		fmt.Println(errors.New("You must provide a user with the --user flag to use updateforks"))
-		os.Exit(1)
-	}
 
 	if *token == "" {
 		if os.Getenv("GITHUB_TOKEN") == "" {
@@ -239,24 +44,188 @@ func main() {
 		*token = os.Getenv("GITHUB_TOKEN")
 	}
 
-	gh := &Github{
-		URI:        *uri,
-		APIVersion: *apiVersion,
-		user:       *user,
-		token:      *token,
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: *token},
+	)
+	tc := oauth2.NewClient(oauth2.NoContext, ts)
+
+	client = github.NewClient(tc)
+}
+
+type Repositories interface {
+	List(string, *github.RepositoryListOptions) ([]*github.Repository, *github.Response, error)
+	Get(string, string) (*github.Repository, *github.Response, error)
+	GetBranch(string, string, string) (*github.Branch, *github.Response, error)
+}
+
+func getAllRepositories(r Repositories) ([]*github.Repository, error) {
+	repos, _, err := r.List("", nil)
+	if err != nil {
+		return nil, fmt.Errorf("could not list repositories: %v", err)
+	}
+	return repos, nil
+}
+
+func updateForkUpstream(r Repositories, f *Fork) error {
+	found, _, err := r.Get(f.Owner, f.Name)
+	if err != nil {
+		return fmt.Errorf("error getting repository: %s", err)
+	}
+	f.UpstreamOwner = *found.Parent.Owner.Login
+	return nil
+}
+
+func getBranchSHA(r Repositories, owner, name, branch string) (string, error) {
+	found, _, err := r.GetBranch(owner, name, branch)
+	if err != nil {
+		return "", fmt.Errorf("error getting fork branch: %s", err)
+	}
+	return *found.Commit.SHA, nil
+}
+
+func isForkUpToDateWithUpstream(r Repositories, f Fork) (bool, error) {
+	forkSHA, err := getBranchSHA(r, f.Owner, f.Name, f.DefaultBranch)
+	if err != nil {
+		return false, fmt.Errorf("error getting fork branch: %s", err)
 	}
 
-	err := gh.GetRepos()
+	upstreamSHA, err := getBranchSHA(r, f.UpstreamOwner, f.Name, f.DefaultBranch)
 	if err != nil {
-		fmt.Println(os.Stderr, err)
+		return false, fmt.Errorf("error getting upstream branch: %s", err)
+	}
+
+	if forkSHA == upstreamSHA {
+		return true, nil
+	}
+
+	return false, nil
+}
+
+func gitexec(args []string) error {
+	cmd := exec.Command("git", args...)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		return err
+	}
+
+	if len(output) > 0 {
+		fmt.Println(string(output))
+	}
+
+	return nil
+}
+
+func generateSSHURL(owner, name string) string {
+	return "git@github.com:" + owner + "/" + name + ".git"
+}
+
+func Clone(f Fork, dir string) error {
+	cloneSSHURL := generateSSHURL(f.Owner, f.Name)
+	return gitexec([]string{"clone", "--depth", "1", cloneSSHURL, dir})
+}
+
+func AddUpstream(f Fork) error {
+	upstreamSSHURL := generateSSHURL(f.UpstreamOwner, f.Name)
+	return gitexec([]string{"remote", "add", "upstream", upstreamSSHURL})
+}
+
+func UpdateRemotes() error {
+	return gitexec([]string{"remote", "-v", "update", "-p"})
+}
+func RebaseForkFromUpstream(f Fork) error {
+	return gitexec([]string{"rebase", "upstream/" + f.DefaultBranch})
+}
+
+func PushFork(f Fork) error {
+	return gitexec([]string{"push", "origin", f.DefaultBranch})
+}
+
+func updateFork(fork Fork, dir string) error {
+	var err error
+	err = Clone(fork, dir)
+	if err != nil {
+		return fmt.Errorf("could not clone fork %s: %v", fork.FullName, err)
+	}
+
+	err = os.Chdir(dir)
+	if err != nil {
+		return fmt.Errorf("could not change dir: %s", err)
+	}
+
+	err = AddUpstream(fork)
+	if err != nil {
+		return fmt.Errorf("could not add upstream url for %s: %v", fork.FullName, err)
+	}
+
+	err = UpdateRemotes()
+	if err != nil {
+		return fmt.Errorf("could not update remote: %s", err)
+	}
+
+	err = RebaseForkFromUpstream(fork)
+	if err != nil {
+		return fmt.Errorf("could not rebase fork from upstream: %s", err)
+	}
+
+	err = PushFork(fork)
+	if err != nil {
+		return fmt.Errorf("could not push to remote: %s", err)
+	}
+
+	return nil
+
+}
+func main() {
+
+	repos, err := getAllRepositories(client.Repositories)
+	if err != nil {
+		fmt.Errorf("could not get all repositories: %s", err)
 		os.Exit(1)
 	}
 
-	err = gh.updateForks()
-	if err != nil {
-		fmt.Println(os.Stderr, err)
-		os.Exit(1)
+	forks := new([]Fork)
+
+	for _, repo := range repos {
+		if !*repo.Fork {
+			continue
+		}
+		fork := &Fork{
+			Name:          *repo.Name,
+			FullName:      *repo.FullName,
+			Owner:         *repo.Owner.Login,
+			DefaultBranch: *repo.DefaultBranch,
+		}
+
+		updateForkUpstream(client.Repositories, fork)
+		if err != nil {
+			log.Printf("could not get fork upstream for %s: %v", fork.Name, err)
+			continue
+		}
+
+		*forks = append(*forks, *fork)
 	}
 
-	fmt.Println("All forks have been updated.")
+	for _, fork := range *forks {
+		upToDate, err := isForkUpToDateWithUpstream(client.Repositories, fork)
+
+		if upToDate {
+			continue
+		}
+
+		fmt.Println(fork.Name, ":", fork.Owner, "commit does not match", fork.UpstreamOwner, "commit")
+
+		dir, err := ioutil.TempDir("", "tmp")
+		if err != nil {
+			fmt.Errorf("could not create a temporary directory: %s", err)
+			os.Exit(1)
+		}
+		defer os.RemoveAll(dir)
+
+		err = updateFork(fork, dir)
+		if err != nil {
+			fmt.Println("could not update fork %s: %v", fork.Name, err)
+			os.Exit(1)
+		}
+
+	}
 }
