92fca173dc52fd2c22def1b15b31755557bacb58
Chase Adams <hey@chaseadams.io>
2018-02-13 20:27:53 -0800

    zsh: add aliases for quick folder changing

--- zshrc.zsh
+++ zshrc.zsh
@@ -33,8 +33,11 @@ export LS_COLORS='no=00:fi=00:di=01;34:ln=01;36:pi=40;33:so=01;35:do=01;35:bd=40
 
 ### SHORTCUTS ###
 alias godf="cd $DOTFILES"
+alias gogl="cd $WRK/gitlab.com"
+alias goglca="cd $WRK/gitlab.com/chaseadamsio"
+alias goghca="cd $WRK/github.com/chaseadamsio"
 
-alias re!=". $HOME/.bash_profile"
+alias re!=". $HOME/.zshrc"
 
 ### DOCKER ###
 # stop all running docker containers:
