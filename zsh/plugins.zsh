357f90d5519bdfe4c52e160b13715c0630a61b29
Chase Adams <realchaseadams@gmail.com>
2020-04-18 23:59:28 -0700

    add antibody to zsh

---
+++ zsh/plugins.zsh
@@ -0,0 +1,7 @@
+#!/bin/zsh
+
+antibody bundle mafredri/zsh-async
+antibody bundle sindresorhus/pure
+antibody bundle zsh-users/zsh-autosuggestions
+antibody bundle zsh-users/zsh-syntax-highlighting
+antibody bundle rupa/z
